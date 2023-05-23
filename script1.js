2606987f82dedacbc54d8a3826b196c2e086dc4b
niks321 <134275611+niks321@users.noreply.github.com>
2023-05-23 09:54:37 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+var a = 10;
+var b=a++;
+var c=a+b;
+var d=a*b;
+var e=a-b;
+var f=a/b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(a++);
